78a344011547aea789fefcc51a00e5cf7840f003
clairernied <clairenied@gmail.com>
2017-12-23 16:48:10 -0600

    fixing style string tabs

--- .next/dist/app/components/Header.js
+++ .next/dist/app/components/Header.js
@@ -22,7 +22,7 @@ var _jsxFileName = "/Users/claireniederberger/repos/wt/app/components/Header.js"
 
 exports.default = function () {
   return _react2.default.createElement("div", {
-    className: "jsx-343709902" + " " + "header-container",
+    className: "jsx-2037562644" + " " + "header-container",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 5
@@ -32,7 +32,7 @@ exports.default = function () {
       lineNumber: 6
     }
   }, _react2.default.createElement("a", {
-    className: "jsx-343709902",
+    className: "jsx-2037562644",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 7
@@ -42,14 +42,14 @@ exports.default = function () {
       lineNumber: 9
     }
   }, _react2.default.createElement("a", {
-    className: "jsx-343709902",
+    className: "jsx-2037562644",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 10
     }
   }, "About")), _react2.default.createElement(_style2.default, {
-    styleId: "343709902",
-    css: ".header-container.jsx-343709902{width:100%;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between;}.header-container.jsx-343709902>a.jsx-343709902{margin-right:14px;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFZTyxBQUdvQixBQU1XLFdBTFQsT0FNYixtRUFMa0MsbUhBQ2xDIiwiZmlsZSI6ImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvY2xhaXJlbmllZGVyYmVyZ2VyL3JlcG9zL3d0Iiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IExpbmsgZnJvbSAnbmV4dC9saW5rJztcblxuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxkaXYgY2xhc3NOYW1lPVwiaGVhZGVyLWNvbnRhaW5lclwiPlxuICAgIDxMaW5rIGhyZWY9XCIvXCI+XG4gICAgICA8YT5FcGlzb2RlczwvYT5cbiAgICA8L0xpbms+XG4gICAgPExpbmsgaHJlZj1cIi9hYm91dFwiPlxuICAgICAgPGE+QWJvdXQ8L2E+XG4gICAgPC9MaW5rPlxuICAgIDxzdHlsZSBqc3g9XCJ0cnVlXCI+XG4gICAgICB7YFxuICAgICAgICAuaGVhZGVyLWNvbnRhaW5lciB7XG4gICAgICAgIHdpZHRoOiAxMDAlO1xuICAgICAgICBkaXNwbGF5OiBmbGV4O1xuICAgICAgICAgICAganVzdGlmeS1jb250ZW50OiBzcGFjZS1iZXR3ZWVuO1xuICAgICAgICB9XG5cbiAgICAgICAgLmhlYWRlci1jb250YWluZXIgPiBhIHtcbiAgICAgICAgICAgIG1hcmdpbi1yaWdodDogMTRweDtcbiAgICAgICAgfVxuICAgICAgYH1cbiAgICA8L3N0eWxlPlxuICA8L2Rpdj5cbik7XG4iXX0= */\n/*@ sourceURL=app/components/Header.js */"
+    styleId: "2037562644",
+    css: ".header-container.jsx-2037562644{width:100%;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between;}.header-container.jsx-2037562644>a.jsx-2037562644{margin-right:14px;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFZTyxBQUdzQixBQU1PLFdBTEwsT0FNZixtRUFMZ0MsbUhBQ2hDIiwiZmlsZSI6ImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvY2xhaXJlbmllZGVyYmVyZ2VyL3JlcG9zL3d0Iiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IExpbmsgZnJvbSAnbmV4dC9saW5rJztcblxuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxkaXYgY2xhc3NOYW1lPVwiaGVhZGVyLWNvbnRhaW5lclwiPlxuICAgIDxMaW5rIGhyZWY9XCIvXCI+XG4gICAgICA8YT5FcGlzb2RlczwvYT5cbiAgICA8L0xpbms+XG4gICAgPExpbmsgaHJlZj1cIi9hYm91dFwiPlxuICAgICAgPGE+QWJvdXQ8L2E+XG4gICAgPC9MaW5rPlxuICAgIDxzdHlsZSBqc3g9XCJ0cnVlXCI+XG4gICAgICB7YFxuICAgICAgICAuaGVhZGVyLWNvbnRhaW5lciB7XG4gICAgICAgICAgd2lkdGg6IDEwMCU7XG4gICAgICAgICAgZGlzcGxheTogZmxleDtcbiAgICAgICAgICBqdXN0aWZ5LWNvbnRlbnQ6IHNwYWNlLWJldHdlZW47XG4gICAgICAgIH1cblxuICAgICAgICAuaGVhZGVyLWNvbnRhaW5lciA+IGEge1xuICAgICAgICAgIG1hcmdpbi1yaWdodDogMTRweDtcbiAgICAgICAgfVxuICAgICAgYH1cbiAgICA8L3N0eWxlPlxuICA8L2Rpdj5cbik7XG4iXX0= */\n/*@ sourceURL=app/components/Header.js */"
   }));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyJdLCJuYW1lcyI6WyJMaW5rIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFHUDs7Ozs7Ozs7a0JBQWUsWUFBQTt5QkFDYixjQUFBO3VDQUFBLEFBQWU7O2dCQUFmO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUE7ZUFBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBRkosQUFDRSxBQUNFLEFBRUYsOEJBQUEsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUE7ZUFBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBTEosQUFJRSxBQUNFO2FBTEo7U0FEYSxBQUNiO0FBQUE7QUFERiIsImZpbGUiOiJIZWFkZXIuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2NsYWlyZW5pZWRlcmJlcmdlci9yZXBvcy93dCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9jb21wb25lbnRzL0hlYWRlci5qcyJdLCJuYW1lcyI6WyJMaW5rIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFHUDs7Ozs7Ozs7a0JBQWUsWUFBQTt5QkFDYixjQUFBO3dDQUFBLEFBQWU7O2dCQUFmO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUE7ZUFBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBRkosQUFDRSxBQUNFLEFBRUYsOEJBQUEsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUE7ZUFBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBTEosQUFJRSxBQUNFO2FBTEo7U0FEYSxBQUNiO0FBQUE7QUFERiIsImZpbGUiOiJIZWFkZXIuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2NsYWlyZW5pZWRlcmJlcmdlci9yZXBvcy93dCJ9
--- app/components/Header.js
+++ app/components/Header.js
@@ -12,13 +12,13 @@ export default () => (
     <style jsx="true">
       {`
         .header-container {
-        width: 100%;
-        display: flex;
-            justify-content: space-between;
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
         }
 
         .header-container > a {
-            margin-right: 14px;
+          margin-right: 14px;
         }
       `}
     </style>
